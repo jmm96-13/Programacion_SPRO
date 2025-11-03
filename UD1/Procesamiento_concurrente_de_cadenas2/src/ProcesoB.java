e577eda5c03217d1b84e36e71ab43cd5afdf554d
jmm96-13 <jmm.cv.96@gmail.com>
2025-11-03 13:21:34 +0100

    Update ProcesoB.java

--- UD1/Procesamiento_concurrente_de_cadenas2/src/ProcesoB.java
+++ UD1/Procesamiento_concurrente_de_cadenas2/src/ProcesoB.java
@@ -4,19 +4,20 @@ import java.io.PrintWriter;
 
 public class ProcesoB { //contador de caracteres
     public static void main(String[] args) {
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
-            //lector de la entrada estandar BufferedReader lee las líneas que llegan del Proceso A (por la entrada estándar)
-             PrintWriter out = new PrintWriter(System.out, true)) { 
-            //escritor de la salida estandar PrintWriter escribe las líneas procesadas para el Proceso C (por la salida estándar)
+        try (BufferedReader lector = new BufferedReader(new InputStreamReader(System.in)); 
+            //lector de la entrada estandar BufferedReader lee las líneas que llegan del Proceso A
+             PrintWriter escritor = new PrintWriter(System.out, true)) { 
+            //escritor de la salida estandar, PrintWriter escribe las líneas procesadas para el Proceso C
 
             String linea; //variable para almacenar cada línea leída
-            while ((linea = in.readLine()) != null) { 
-                //lee línea por línea hasta el final de la entrada
-                int len = linea.length(); //calcula la longitud de la línea
-                out.println(linea + "," + len); //escribe la línea original separada por una coma
+            while ((linea = lector.readLine()) != null) { 
+                //lee línea por línea hasta el final de la entrada, hasta que no haya más datos
+                int longitud = linea.length(); //calcula la longitud de la línea
+                escritor.println(linea + "," + longitud); //escribe la línea original separada por una coma
             }
+            
         } catch (Exception e) {
-            e.printStackTrace(System.err);
+            e.printStackTrace(System.err); //muestra errores de escritura o lectura
         }
     }
 }
