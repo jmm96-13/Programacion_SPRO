03e6228e893eda3b579c7e4f7b87d4f4f32e1c70
jmm96-13 <jmm.cv.96@gmail.com>
2025-11-03 13:06:03 +0100

    Refactor variable names for clarity in ProcesoA

--- UD1/Procesamiento_concurrente_de_cadenas2/src/ProcesoA.java
+++ UD1/Procesamiento_concurrente_de_cadenas2/src/ProcesoA.java
@@ -30,20 +30,20 @@ import java.util.Random;
 public class ProcesoA { //generador de cadenas
     public static void main(String[] args) {
         String alfabeto = "abcdefghijklmnñopqrstuvwxyz";
-        Random random = new Random(); // Generador de números aleatorios 
-        PrintWriter pw = new PrintWriter(System.out, true); // PrintWriter envia texto por salida estándar (System.out)
+        Random aleatorio = new Random(); // Generador de números aleatorios 
+        PrintWriter salida = new PrintWriter(System.out, true); // PrintWriter envia texto por salida estándar (System.out)
                                                                       //true activa auto-flush y envia cada línea al Proceso B
                                                                       //Asi no necesito OutputStream os ya que System.out lo es
         for (int i = 0; i < 15; i++) { //Bucle para 15 cadenas
-            int longitud = 5 + random.nextInt(11); //elige aleatoriamente la longitud de la cadena entre 5–15 
-            StringBuilder sb = new StringBuilder(longitud); //constructor StringBuilder cadena carácter a carácter
+            int longitud = 5 + aleatorio.nextInt(11); //elige aleatoriamente la longitud de la cadena entre 5–15 
+            StringBuilder palabra = new StringBuilder(longitud); //constructor StringBuilder cadena carácter a carácter
             
             for (int j = 0; j < longitud; j++) { // Bucle que genera cada carácter aleatorio
-                sb.append(alfabeto.charAt(random.nextInt(alfabeto.length()))); // se añade esa letra aleatoria a la cadena
+                palabra.append(alfabeto.charAt(aleatorio.nextInt(alfabeto.length()))); // se añade esa letra aleatoria a la cadena
             }
-            pw.println(sb.toString()); //envía la cadena generada al proceso B por la salida estándar
+            salida.println(palabra.toString()); //envía la cadena generada al proceso B por la salida estándar
         }
     }
 }
 
-//Genera 15 cadenas aleatorias de letras (a–z), de longitud entre 5-15 caracteres.
+//Genera 15 cadenas aleatorias de letras (a–z), de longitud entre 5-15 caracteres, elegido por Random aleatorio.
